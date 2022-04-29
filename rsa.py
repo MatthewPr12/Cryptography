382c41d3174f849f0a8fc2195ef6c2b182e0b245
NaniiiGock <91615473+NaniiiGock@users.noreply.github.com>
2022-04-29 16:41:26 +0300

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -3,58 +3,52 @@ from hashlib import sha256
 from secrets import compare_digest
 from primes import primes
 
-
 def gcd(a, b):
-    if b == 0:
+    if (b == 0):
         return a
     else:
         return gcd(b, a % b)
 
-
-def extended_gcd(e, fi):
+def extended_gcd(e, fi_arg):
+    fi = fi_arg
     x, x1, y, y1 = 0, 1, 1, 0
-    while fi != 0:
-        coeff = e // fi
-        e, fi = fi, e - coeff * fi
-        x1, x = x, x1 - coeff * x
-        y1, y = y, y1 - coeff * y
-    if x1 < 0:
-        x1 += fi
-    return x1
+    while fi:
+        coeff = e//fi
+        e, fi = fi, e-coeff*fi
+        x, x1 = x1 - coeff*x, x
+        y, y1 = y1 - coeff*y, y
+    return x1%fi_arg
 
 
 def create_e(fi):
-    for i in range(len(primes)):  # або змінити на бін пошук
+    for i in range(len(primes)): # або змінити на бін пошук
         if primes[i] >= fi:
             break
     while True:
-        e = primes[random.randint(0, i)]
+        e = primes[random.randint(0,i)]
         if gcd(e, fi) == 1:
             return e
-
 
 def create_keys():
     n = 0
     while n <= 1500:
-        p1 = primes[random.randint(30, 100)]
-        p2 = primes[random.randint(30, 100)]
+        p1 = primes[random.randint(10, 20)]
+        p2 = primes[random.randint(10, 30)]
         while p1 == p2:
-            p2 = primes[random.randint(10, 30)]
+            p2 = primes[random.randint(10,30)]
         n = p1 * p2
-    fi = (p1 - 1) * (p2 - 1)
+    fi= (p1-1)*(p2-1)
     e = create_e(fi)
     d = extended_gcd(e, fi)
     return n, e, d
 
-
 def encrypt(message, en):
     e, n = en
-    blocks = []
+    blocks= []
     for i in range(len(message)):
         number = (ord(message[i]) ** e) % n
         blocks.append(str(number))
     return " ".join(blocks)
-
 
 def decrypt(blocks, dn):
     d, n = dn
@@ -64,20 +58,19 @@ def decrypt(blocks, dn):
         message += chr(letter_index)
     return message
 
-
 def main():
     """check with hashing"""
 
     n, e, d = create_keys()
     print(n, e, d)
 
-    m = "тестування проги mova 233,є1=щч1\їусщк"
+    m = "lrkngdlknf;wlrjmclfj"
     sha256_digest_1 = sha256(m.encode("utf-8"))
     digest_1 = sha256_digest_1.digest()
     hexdigest_1 = sha256_digest_1.hexdigest()
 
     encrypted = encrypt(m, (e, n))
-    print("ENCRYPTED", encrypted)
+    print("ENCRYPTED",encrypted)
     decrypted = decrypt(encrypted, (d, n))
     print("DECRYPTED", decrypted)
 
@@ -86,7 +79,6 @@ def main():
     hexdigest_2 = sha256_digest_2.hexdigest()
     print(compare_digest(digest_1, digest_2))
     print(compare_digest(hexdigest_1, hexdigest_2))
-
 
 if __name__ == "__main__":
     main()
